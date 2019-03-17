5daaf100f1d1f6dbe95a7574384b9296779dece9
Jack Mallon <jack.mallonk@gmail.com>
2019-03-17 21:06:50 +0000

    Added the rest of the public pages and basic authentication

--- src/Controller/PublicController.php
+++ src/Controller/PublicController.php
@@ -8,13 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 class PublicController extends AbstractController
 {
     /**
-     * @Route("/", name="public")
+     * @Route("/", name="home")
      */
     public function index()
     {
-        return $this->render('public/index.html.twig', [
-            'controller_name' => 'PublicController',
-        ]);
+        return $this->render('public/index.html.twig');
     }
 
     /**
@@ -22,8 +20,22 @@ class PublicController extends AbstractController
      */
     public function about()
     {
-        return $this->render('public/about.html.twig', [
-            'controller_name' => 'PublicController',
-        ]);
+        return $this->render('public/about.html.twig');
+    }
+
+    /**
+     * @Route("/upcoming", name="upcoming")
+     */
+    public function upcoming()
+    {
+        return $this->render('public/upcoming.html.twig');
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contact()
+    {
+        return $this->render('public/contact.html.twig');
     }
 }

c478b3bc0153edd76b41defe15c4e57ee26bdea4
Jack Mallon <jack.mallonk@gmail.com>
2019-03-20 15:30:38 +0000

    added all pages to dashboard

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,10 +10,41 @@ class AdminController extends AbstractController
 {
     /**
      * @Route("/admin", name="admin")
-     * @IsGranted("ROLE_ADMIN")
      */
     public function index()
     {
         return $this->render('admin/index.html.twig');
+    }
+
+    /**
+     * @Route("/admin/upcoming", name="admin_upcoming")
+     */
+    public function upcoming()
+    {
+        return $this->render('admin/upcoming.html.twig');
+    }
+
+    /**
+     * @Route("/admin/create-referendum", name="create_referendum")
+     */
+    public function create_referendum()
+    {
+        return $this->render('admin/create-referendum.html.twig');
+    }
+
+    /**
+     * @Route("/admin/past-referendums", name="past_referendums")
+     */
+    public function past_referendum()
+    {
+        return $this->render('admin/past-referendums.html.twig');
+    }
+
+    /**
+     * @Route("/admin/manage-administrators", name="manage_admins")
+     */
+    public function manage_administrators()
+    {
+        return $this->render('admin/manage-admins.html.twig');
     }
 }
